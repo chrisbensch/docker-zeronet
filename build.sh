ed64c61b3a57ae434b4807da0b097632458df9ca
Chris Bensch <chris.bensch@gmail.com>
2022-09-18 14:30:43 -0700

    Testing build.sh

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ cp ../docker-compose.yml docker-compose.yml
 cp ../Dockerfile.integrated_tor Dockerfile.integrated_tor
 cp ../torrc.txt torrc
 
+docker build . -F Dockerfile.integrated_tor -t chrisbensch/docker-zeronet:latest
